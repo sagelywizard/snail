Fix numeric stability for causal attention

snail.py
@@ -129,8 +129,11 @@ class AttentionBlock(nn.Module):
         # Use numpy triu because you can't do 3D triu with PyTorch
         # TODO: using float32 here might break for non FloatTensor inputs.
         # Should update this later to use numpy/PyTorch types of the input.
-        numpy_mask = np.triu(np.ones(logits.size()), k=1).astype('float32')
-        causal_mask = Variable(torch.from_numpy(numpy_mask)*float('-inf'))
-        probs = F.softmax(logits+causal_mask) / self.sqrt_k
+        mask = np.triu(np.ones(logits.size()), k=1).astype('uint8')
+        mask = torch.from_numpy(mask)
+        # do masked_fill_ on data rather than Variable because PyTorch doesn't
+        # support masked_fill_ w/-inf directly on Variables for some reason.
+        logits.data.masked_fill_(mask, float('-inf'))
+        probs = F.softmax(logits, dim=1) / self.sqrt_k
         read = torch.bmm(probs, values)
         return torch.cat([minibatch, read], dim=2)